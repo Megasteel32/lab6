2efb13385e219642c76d5292b77af7e6299de600
Megasteel32 <megasteel32@gmail.com>
2020-09-28 22:08:08 -0500

    Initial commit

---
+++ program1.py
@@ -0,0 +1,8 @@
+enter_pokemon = 1
+pokedex = {}
+while enter_pokemon == 1:
+    pokemon = input("WHO'S THAT POKEMON?" )
+    current_cp = int(input("What's its CP?" ))
+    current_level = int(input("What's its level? "))
+    candies = int(input("How many candies? "))
+    
