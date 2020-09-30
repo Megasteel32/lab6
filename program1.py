31def5ae220364478eb3a5e47c602640b1d0ecb3
Megasteel32 <megasteel32@gmail.com>
2020-09-30 08:25:05 -0500

    bruh

--- program1.py
+++ program1.py
@@ -1,3 +1,13 @@
+# By submitting this assignment, I agree to the following:
+#  Aggies do not lie, cheat, or steal, or tolerate those who do�
+#  I have not given or received any unauthorized aid on this assignment�
+#
+# Name:         Anthony Matl, Luca Maddaleni, Landon Charles, Nathaniel Michaud
+# Group:        8
+# Section:      273
+# Assignment:   Lab 6 Program 1
+# Date:         9/30/2020
+
 pokedex = {}
 pokemon = input("WHO'S THAT POKEMON? ")
 current_cp = int(input("What's its CP? "))
