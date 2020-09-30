3cba87fb233d3335ef964d2bdcbcbfce36948148
Megasteel32 <megasteel32@gmail.com>
2020-09-30 08:30:23 -0500

    bruh

--- program1.py
+++ program1.py
@@ -8,18 +8,23 @@
 # Assignment:   Lab 6 Program 1
 # Date:         9/30/2020
 
+# Defining dictionary and asking for input from users
 pokedex = {}
 pokemon = input("WHO'S THAT POKEMON? ")
 current_cp = int(input("What's its CP? "))
 current_level = int(input("What's its level? "))
 candies = int(input("How many candies? "))
+
+# Adding user input to dictionary
 pokedex["Pokemon"] = pokemon
 pokedex["Current CP"] = current_cp
 pokedex["Current Level"] = current_level
 pokedex["Candies"] = candies
 
+# Main loop
 user_input = 0
 while user_input != 3:
+    # Printing out menu with stored values
     print("Pokemon: {}".format(pokedex["Pokemon"]))
     print("Current CP: {}".format(pokedex["Current CP"]))
     print("Current Level: {}".format(pokedex["Current Level"]))
@@ -29,18 +34,24 @@ while user_input != 3:
     print("3 - Exit to Main Menu")
     user_input = int(input("Enter 1, 2, or 3: "))
     if user_input == 1:
+        # Adding user inputted candies to dictionary
         candies = int(input("How many candies would you like to add? "))
         pokedex["Candies"] += candies
     if user_input == 2:
+        # Checking for current level
         if pokedex["Current Level"] < 31 and pokedex["Candies"] > 0:
+            # Adding correct CP if the level is between 1 and 30
             pokedex["Candies"] -= 1
             pokedex["Current Level"] += 1
             pokedex["Current CP"] = (pokedex["Current CP"] * 0.0094) / (0.095 * pokedex["Current CP"]**(1/2))**2
         elif pokedex["Current Level"] < 40 and pokedex["Candies"] > 1:
+            # Adding correct CP if the level is between 31 and 40
             pokedex["Candies"] -= 2
             pokedex["Current Level"] += 1
             pokedex["Current CP"] = (pokedex["Current CP"] * 0.0045) / (0.095 * pokedex["Current CP"] ** (1 / 2)) ** 2
         elif pokedex["Current Level"] == 40:
+            # If the pokemon is at level 40, no CP is added
             print("Maximum level is 40!")
         else:
+            # If the user doesn't have enough candies for the level upgrade needed, tell them so
             print("You need more candies!")
